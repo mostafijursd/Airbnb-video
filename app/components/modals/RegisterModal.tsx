5711d3954e961c85eb6471dd84c954810209ea35
Mostafijur Rahaman <mostafijursd@users.noreply.github.com>
2024-07-16 22:18:27 +0530

    add api,inputs,useLoginModal,libs,pages,prisma

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,9 +9,13 @@ import {
   useForm
 } from "react-hook-form";
 
-
+import { signIn } from 'next-auth/react';
 import Modals from './Modal';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import Button from '../Button';
+import toast from 'react-hot-toast';
 
 
 const RegisterModal = () => {
@@ -40,11 +44,11 @@ const RegisterModal = () => {
     axios.post('/api/register', data)
    .then(()=>{
    
+    toast.success("Success!")
     registerModal.onClose();
-  
    })
   .catch((error)=>{
-    console.log(error);
+   toast.error("Something went Wrong!")
     
     
   })
@@ -52,6 +56,77 @@ const RegisterModal = () => {
     setIsLoading(false)
   })
   }
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+   // loginModal.onOpen();
+  }, [registerModal])
+
+  const bodyContent=(
+    <div className=' flex flex-col gap-4'>
+  <Heading 
+  title='Welcome to Airbnb'
+ subtitle='Create an Account!'
+  />
+
+<Input
+ id='email'
+ label='Email'
+ disabled={isLoading}
+ register={register}
+ errors={errors}
+ required
+
+/>
+<Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+  const footerContent =(
+    <div className=' flex flex-col gap-4 mt-3 '>
+<hr/>
+ <Button
+ outline
+ label='Continue with Google'
+ icon={FcGoogle}
+ onClick={()=>signIn('google')}
+ />
+<Button 
+ outline
+ label='Continue with Github'
+ icon={AiFillGithub}
+ onClick={()=>signIn('github')}
+ />
+<div className=' text-neutral-500 text-center mt-4 font-light'>
+<div className=' flex flex-row items-center justify-center gap-2'>
+<div>
+ Already have an account ?
+</div>
+<div className=' text-neutral-800 cursor-pointer hover:underline' 
+onClick={onToggle}
+>
+ log in
+</div>
+
+</div>
+</div>
+    </div>
+ )
+
   return (
     <Modals  
     disabled={isLoading}
@@ -60,9 +135,11 @@ const RegisterModal = () => {
     actionLabel='Continue'
     onClose={registerModal.onClose}
     onSubmit={handleSubmit(onSubmit)}
+    body={bodyContent}
+    footer={footerContent}
 
    />
   )
 }
 
-export default RegisterModal
+export default RegisterModal;
