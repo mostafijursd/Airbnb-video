addaa14ebc75b283c2a921dda8f2347ff2454ded
Mostafijur Rahaman <mostafijursd@users.noreply.github.com>
2024-08-04 20:56:04 +0530

    ad usemenu, layout,page,getLisings,heatBitton, useFavorite

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -37,7 +37,7 @@ SetIsOpen((value)=>!value);
   return (
     <div className=" relative">
 <div className=" flex flex-row items-center gap-3 ">
-<div className=" hidden md:block text-sm font-semibold py-3 px-4 
+<div onClick={onReact} className=" hidden md:block text-sm font-semibold py-3 px-4 
 rounded-full hover:bg-neutral-100 cursor-pointer">
 Airbnb your Home
 </div>
